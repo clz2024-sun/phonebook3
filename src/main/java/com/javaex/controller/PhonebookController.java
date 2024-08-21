e4415a7f68fade2d8f4a61ba115889067ddac8aa
clz2024.sun <clz2024.sun@gmail.com>
2024-08-21 12:40:13 +0900

    2024-08-21

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -1,29 +1,92 @@
 package com.javaex.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.service.PhonebookService;
+import com.javaex.vo.PersonVo;
 
 @Controller
 public class PhonebookController {
 
-	//필드
-	//생성자
-	//메소드gs
-	//메소드일반/////////////////////////////
 	
-	//등록폼
-	@RequestMapping(value="/writeform", method = {RequestMethod.GET, RequestMethod.POST}  )
+	
+	@Autowired
+	private PhonebookService phonebookService;
+	
+	// 생성자
+	// 메소드gs
+
+	// 메소드일반/////////////////////////////
+	
+	/* 삭제 */
+	@RequestMapping(value="/delete", method = {RequestMethod.GET, RequestMethod.POST})
+	public String delete(@RequestParam(value="no") int no) {
+		System.out.println("PhonebookController.delete()");
+		
+		phonebookService.exePersonDelete(no);
+	
+		return "redirect:/list";
+	}
+	
+	/* 리스트 */
+	@RequestMapping(value = "/list", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		System.out.println("PhonebookController.list()");
+
+		List<PersonVo> personList = phonebookService.exeGetPerson();
+		model.addAttribute("personList", personList);
+		
+		return "list";
+	}
+
+	/* 등록폼 */
+	@RequestMapping(value = "/writeform", method = { RequestMethod.GET, RequestMethod.POST })
 	public String writeForm() {
 		System.out.println("PhonebookController.writeForm()");
 		
-		return "/WEB-INF/views/writeForm.jsp";
+		return "writeForm";
 	}
+
+	/* 등록 */
+	/* vo로 받을때 */
+	@RequestMapping(value = "write", method = { RequestMethod.GET, RequestMethod.POST })
+	public String write(@ModelAttribute PersonVo personVo) {
+		System.out.println("PhonebookController.write()");
+
+		// phonebookDao의 메소드를 이용해서 저장한다
+		//int count = phonebookDao.insertPerson(personVo);
+		//System.out.println(count);
 	
-	//등록
-	
-	
-	//리스트
-	
-	
+		phonebookService.exeWritePerson(personVo);
+		
+		
+		// 리스트로 리다이렉트
+		/* "redirect:http://localhost:8888/phonebook3/list" */
+		return "redirect:/list";
+		
+	}
+
+	/* 등록 */
+	/* 파라미터 1개씩 받을때 */
+	@RequestMapping(value = "/write2", method = { RequestMethod.GET, RequestMethod.POST })
+	public String write2(@RequestParam(value = "name") String name, @RequestParam("hp") String hp,
+			@RequestParam("company") String company) {
+		System.out.println("PhonebookController.write2()");
+
+		PersonVo personVo = new PersonVo();
+		System.out.println(personVo);
+
+		return "";
+	}
+
+	// 리스트
+
 }
---
+++ src/main/java/com/javaex/service/PhonebookService.java
@@ -0,0 +1,75 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.PhonebookDao;
+import com.javaex.vo.PersonVo;
+
+@Service
+public class PhonebookService {
+	
+	@Autowired
+	private PhonebookDao phonebookDao;
+	
+	/* 전화번호등록 */
+	public int exeWritePerson(PersonVo personVo) {
+		System.out.println("PhonebookService.exeWritePerson()");
+		
+		int count = phonebookDao.insertPerson(personVo);
+		
+		return count;
+	}
+	
+	/* 전화번호 전체 가져오기 */
+	public List<PersonVo> exeGetPerson(){
+		System.out.println("PhonebookService.exeGetPerson()");
+		
+		List<PersonVo> personList = phonebookDao.getPersonList();
+		
+		return personList;
+	}
+	
+	
+	
+	public int exePersonDelete(int no) {
+		System.out.println("PhonebookService.exePersonDelete()");
+		
+		int count = phonebookDao.deletePerson(no);
+		
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
